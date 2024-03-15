aa1723a06691274a89d07053976e989e9fa5dd39
mi.____.ng <alstjr9438@naver.com>
2024-03-15 14:31:39 +0900

    feat: npm, stroybook 추가

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,19 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:storybook/recommended',
+		'prettier',
+		'airbnb-base',
+		'plugin:prettier/recommended', // airbnb prettier 추가
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'prettier'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+	},
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,29 @@ import './App.css';
 import { DatePicker } from 'antd';
 
 function App() {
-    const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <DatePicker />
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
+	return (
+		<>
+			<DatePicker />
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src={viteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://react.dev' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</>
+	);
 }
 
 export default App;
