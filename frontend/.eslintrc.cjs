3d5a37f619e440bcd3d16deaf8013725fcee8706
mi.____.ng <alstjr9438@naver.com>
2024-03-17 00:30:47 +0900

    chore: eslint 수정, react-icon 설치

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,38 +1,37 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:storybook/recommended',
-		'prettier',
-	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
-	rules: {
-		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-		'simple-import-sort/imports': [
-			'error',
-			{
-				groups: [
-					// Packages `react` related packages come first.
-					['^react', '^@?\\w'],
-					// Internal packages.
-					['^(@|components)(/.*|$)'],
-					// Side effect imports.
-					['^\\u0000'],
-					// Parent imports. Put `..` last.
-					['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-					// Other relative imports. Put same-folder imports and `.` last.
-					['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-					// Style imports.
-					['^.+\\.?(css)$'],
-				],
-			},
-		],
-		'simple-import-sort/exports': 'error',
-	},
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:storybook/recommended',
+        'prettier',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'simple-import-sort/imports': [
+            'error',
+            {
+                groups: [
+                    // Packages `react` related packages come first.
+                    ['^react', '^@?\\w'],
+                    // Internal packages.
+                    ['^(@|components)(/.*|$)'],
+                    // Side effect imports.
+                    ['^\\u0000'],
+                    // Parent imports. Put `..` last.
+                    ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                    // Other relative imports. Put same-folder imports and `.` last.
+                    ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                    // Style imports.
+                    ['^.+\\.?(css)$'],
+                ],
+            },
+        ],
+        'simple-import-sort/exports': 'error',
+    },
 };
