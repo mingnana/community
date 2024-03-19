f2e410b69bd938394fb568244b80eaf4df3f3c63
mi.____.ng <alstjr9438@naver.com>
2024-03-19 22:21:23 +0900

    chore: vite 추가, title, prettierre 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { DatePicker } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+
+import Router from './router/index';
+
+import './index.css';
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<DatePicker />
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</>
-	);
+    return (
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <Router />
+            </QueryClientProvider>
+        </RecoilRoot>
+    );
 }
 
 export default App;
