85e415dce9dfe4ca96847b602a9e313c1069272b
mi.____.ng <alstjr9438@naver.com>
2024-03-26 17:58:47 +0900

    chore: vercel analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client';
-
-import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+import App from './App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<>
+		<Analytics />
+		<App />
+	</>,
+);
