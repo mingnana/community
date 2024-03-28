66e79a6e0464657c6207f67c36736617eb48a145
mi.____.ng <alstjr9438@naver.com>
2024-03-29 01:39:05 +0900

    fix: 빌드 에러 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
-
 import { Analytics } from '@vercel/analytics/react';
-
 
 import './index.css';
 
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<>
+		<App />
+		<Analytics />
+	</>,
+);
