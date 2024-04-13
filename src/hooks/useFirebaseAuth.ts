cf5f7ea05c1354a62fb692b5f79756c637806b45
mi.____.ng <alstjr9438@naver.com>
2024-04-13 23:09:19 +0900

    feat: 토큰  만료 분기처리

--- src/hooks/useFirebaseAuth.ts
+++ src/hooks/useFirebaseAuth.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { message } from 'antd';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import { IUserInfo } from '@/interfaces/auth';
@@ -7,6 +9,9 @@ import 'firebase/auth';
 import { app } from '../../config';
 
 export const useFirebaseAuth = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
 	const [user, setUser] = useState<IUserInfo | undefined>();
 
 	useEffect(() => {
@@ -15,10 +20,15 @@ export const useFirebaseAuth = () => {
 		const unsubscribe = onAuthStateChanged(auth, (user) => {
 			if (user) {
 				setUser({ email: user.email });
+			} else if (!user && location.pathname !== '/') {
+				message.error('로그인이 필요합니다! 😢');
+				setTimeout(() => {
+					navigate('/');
+				}, 500);
 			}
 		});
 
 		return () => unsubscribe();
-	}, []);
+	}, [location.pathname, navigate]);
 	return { user };
 };
